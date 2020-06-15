aa80506dd394bc8cbdbf468df12c94d28a324363
Mor NDIAYE <mo.ndiaye@ecole-ipssi.net>
2020-06-15 23:04:06 +0200

    [feat] : Put in place a Management area for teams created by a given user
    Next =>:
    - Improve the backend
    - Implement this feature and Improve it the ui step by step as well

--- src/graphql/fragments.js
+++ src/graphql/fragments.js
@@ -2,17 +2,39 @@ import { gql } from 'apollo-boost';
 
 // user fragment
 export const USER_INFOS = gql`
-    fragment userInfos on User {
-        _id
+	fragment userInfos on User {
+		_id
+		name
+		username
+		email
+		images {
+			url
+			public_id
+		}
+		bio
+		createdAt
+		updatedAt
+	}
+`;
+
+// team fragment
+export const TEAM_INFOS = gql`
+	fragment teamsInfosForAUser on Team {
+		_id
+		name
+		description
+		admin {
+			_id
 			name
 			username
 			email
-			images {
-				url
-				public_id
-			}
-			bio
-			createdAt
-			updatedAt
-    }
+		}
+		users {
+			_id
+			name
+			username
+		}
+		createdAt
+		updatedAt
+	}
 `;
